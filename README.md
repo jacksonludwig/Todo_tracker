bb117c7be2e2ee21761043cdfefb59d896e1dc9b
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-02-21 17:20:40 -0500

    edit text transaction

---
+++ README.md
@@ -0,0 +1,8 @@
+* TODO
+
+** Edit task text
+Add event to controller to handle clicking task. 
+
+Make it pop-up a window to allow editing text.
+
+Set the task to that text, but also have use a transaction class to allow undoing.
---
+++ src/todo/transactions/EditTextTask_Transaction.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// IMPORT ALL THE THINGS NEEDED FROM OTHER JAVASCRIPT SOURCE FILES
+import { jsTPS_Transaction } from "../../common/jsTPS.js"
+
+export default class EditTextTask_Transaction extends jsTPS_Transaction {
+    constructor(initModel) {
+        super();
+        this.model = initModel;
+    }
+
+    doTransaction() {
+      // TODO
+    }
+
+    undoTransaction() {
+      // TODO
+    }
+}
